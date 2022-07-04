65add154bdba6c724023e2d310329b7834a6b609
Yifei Yu <48498200+Danielyuuuuu@users.noreply.github.com>
2022-07-04 17:06:08 +1000

    Changed the wordings of some of the prompts

--- src/MovieTheatre.java
+++ src/MovieTheatre.java
@@ -64,39 +64,41 @@ public class MovieTheatre {
                     exit(1);
                     break;
             }
-            System.out.println("---------------------------------------------------");
+            System.out.println("----------------------------------------------------------------------------------------------");
         }
     }
 
     // To handle the initial state where the user can choose between resetting the movie seating and getting the movie listing
     private static void handleInitialState(){
-        System.out.println("Welcome to the movie theater! Please select the following options (Enter option 1 or 2): \n1. Reset the movie seating. \n2. Get the movie listing.");
+        System.out.println("Welcome to the movie theater! Please select the following options (Enter option 1 or 2): \n1. Get the movie listing. \n2. Reset the seating map for all movies.");
         input = scanner.nextLine().trim();
 
-        // Reset the movie seating
-        if(input.equals("1")){
-            movieList.resetMovieSeating();
-            System.out.println("The movie seating has been reset");
-        }
         // Get the movie listing
-        else if(input.equals("2")){
+        if(input.equals("1")){
             currentState = State.SELECT_MOVIE;
+            System.out.println("Option 1 has been selected.");
+        }
+        // Reset the movie seating
+        else if(input.equals("2")){
+            movieList.resetMovieSeating();
+            System.out.println("Option 2 has been selected. The movie seating map has been reset.");
         }
         // Invalid option selected
         else{
-            System.out.println("Error! The option \"" + input + "\" is invalid");
+            System.out.println("Error! The option \"" + input + "\" is invalid.");
         }
     }
 
     // To handle the select move state where the user can choose from the available movies
     private static void handleSelectMovieState(){
-        System.out.println("Please select from the following movies (or enter \"0\" to go back):");
+        System.out.println("Please select from the following movies by entering the movie name (or enter \"0\" to go back):");
         movieList.printAllMovies();
         input = scanner.nextLine().trim();
 
         // Go back to the initial state
         if(input.equals("0")){
             currentState = State.INITIAL_STATE;
+            System.out.println("Going back to the previous state.");
             return;
         }
 
@@ -106,17 +108,18 @@ public class MovieTheatre {
         // Go to the select seat state if the movie is found
         if(currentSelectedMovie != null){
             currentState = State.SELECT_NUMBER_OF_TICKETS;
+            System.out.println("The movie \"" + currentSelectedMovie + "\" has been selected.");
         }
         // Print out the error message if the movie is not found
         else{
-            System.out.println("Error! The movie \"" + input +"\" is not found");
+            System.out.println("Error! The movie \"" + input +"\" is not found.");
         }
     }
 
     // To handle the select number of tickets state where the user can choose how many tickets the person wants to buy
     private static void handleSelectNumberOfTicketsState(){
         // Get the number of tickets the person wants to buy from the command line
-        System.out.println("Please enter the number of tickets that you want to purchase:");
+        System.out.println("Please enter the number of tickets that you want to purchase for the movie \"" + currentSelectedMovie + "\":");
         numberOfTicketsToPurchase = Integer.parseInt(scanner.nextLine());
 
         // Print out the error when the value entered is less than 0
@@ -173,7 +176,7 @@ public class MovieTheatre {
 
         // Get the seat column number from the command line
         if(onlyOneTicket){
-            System.out.println("Please select your seat by entering the row number:");
+            System.out.println("Please select your seat by entering the column number:");
         }
         else{
             System.out.println("Please select ticket number " + ticketNumber + "'s seat by entering the column number:");
